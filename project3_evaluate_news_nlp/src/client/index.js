6a8abf9b73f2abba19407bac52856b6f81477714
Hisham Elreedy <hishamelreedy1@gmail.com>
2021-08-10 18:53:46 +0200

    modify styles and remove unnecessary files

--- project3_evaluate_news_nlp/src/client/index.js
+++ project3_evaluate_news_nlp/src/client/index.js
@@ -2,10 +2,6 @@
 import { checkForURL } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
 
-// console.log(checkForURL);
-
-// alert("I EXIST")
-// console.log("CHANGE!!");
 
 // sass files
 import './styles/resets.scss'

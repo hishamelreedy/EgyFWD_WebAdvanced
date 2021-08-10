965602b8affbd778319c9a49cdc96f3e917fe8e3
Hisham Elreedy <hishamelreedy1@gmail.com>
2021-08-10 13:20:34 +0200

    fixed missing thumbnail issue

--- project1_MyReads A Book Lending App/src/comps/Book.js
+++ project1_MyReads A Book Lending App/src/comps/Book.js
@@ -8,11 +8,14 @@ import React, { Component } from 'react';
 		            return (
 				            <div className="book"> 
 				             <div className="book-top">
-				                <div className="book-cover" style={{
-							                        width: 128,
-								                height: 193,
-								                backgroundImage: `url("${ book.imageLinks.thumbnail }")`
-							             }}/>
+							{book.imageLinks === undefined ?<div className="book-cover" style={{
+																width: 128,
+																height: 193,
+														 }}/> : <div className="book-cover" style={{
+															width: 128,
+															height: 193,
+															backgroundImage: `url("${ book.imageLinks.thumbnail }")`
+													 }}/>}
 				              <div className="book-shelf-changer">
 				                <select defaultValue={book.shelf} onChange={(event) => this.props.onCategoryChange(book, event.target.value)}>              
 				                       <option value="none" disabled>Move to...</option>

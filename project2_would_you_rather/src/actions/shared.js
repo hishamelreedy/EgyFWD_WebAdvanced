6e358aa1d54b97c5d30578787bbb898efec503f9
hishamelreedy <hishamelreedy@gmail.com>
2021-09-24 18:09:59 +0200

    remove loading

--- project2_would_you_rather/src/actions/shared.js
+++ project2_would_you_rather/src/actions/shared.js
@@ -1,17 +1,14 @@
 import { getInitialData } from '../utils/api'
 import { loadingUsers } from '../actions/users'
 import { loadingQuestions } from '../actions/questions'
-import { showLoading, hideLoading } from 'react-redux-loading'
 
 //GETS handInitialData FROM API AND APPLIES IT TO ACTION CREATORS
 export function handleInitialData () {
   return (dispatch) => {
-    dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions })=>{
         dispatch(loadingUsers(users))
         dispatch(loadingQuestions(questions))
-        dispatch(hideLoading())
       })
   }
 }
